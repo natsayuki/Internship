5f7a27b351deee913ef6e6b5dc60c341d88492ba
the42ndturtle <the42ndturtle@gmail.com>
2018-06-02 10:26:19 -0400

    rfid

--- readwrite.py
+++ readwrite.py
@@ -7,38 +7,38 @@ MISO = 24
 SCLK = 25
 
 while 1:
-    try:
-        pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
-        pn532.beign()
-        pn532.SAM_configuration()
+    # try:
+    pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
+    pn532.beign()
+    pn532.SAM_configuration()
 
-        option = input("read or write")
-        if option == "read":
+    option = input("read or write")
+    if option == "read":
 
-            print('Waiting for MiFare card...')
+        print('Waiting for MiFare card...')
 
-            while 1:
-                uid = pn532.read_passive_target()
-                if uid is None:
-                    continue
-                print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
-                data = pn532.mifare_classic_read_block(1)
-                if data is None:
-                    print('Failed to read block 1!')
-                    continue
-                print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
-        elif option == "write":
+        while 1:
             uid = pn532.read_passive_target()
-            CARD_KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-            if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
-                                                           CARD_KEY):
-                print('Error! Failed to authenticate block 4 with the card.')
-                sys.exit(-1)
-            data = bytearray(16)
-            data[0:4] = b'test'
-            if not pn532.mifare_classic_write_block(4, data):
-                print('Error! Failed to write to the card.')
-                sys.exit(-1)
-            print('Wrote card successfully! You may now remove the card from the PN532.')
-    except:
-        print("except")
+            if uid is None:
+                continue
+            print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
+            data = pn532.mifare_classic_read_block(1)
+            if data is None:
+                print('Failed to read block 1!')
+                continue
+            print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
+    elif option == "write":
+        uid = pn532.read_passive_target()
+        CARD_KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+        if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B,
+                                                       CARD_KEY):
+            print('Error! Failed to authenticate block 4 with the card.')
+            sys.exit(-1)
+        data = bytearray(16)
+        data[0:4] = b'test'
+        if not pn532.mifare_classic_write_block(4, data):
+            print('Error! Failed to write to the card.')
+            sys.exit(-1)
+        print('Wrote card successfully! You may now remove the card from the PN532.')
+    # except:
+    #     print("except")
