e13a9d6e7560ab4dbee90124c8e90eae9c2e9245
the42ndturtle <the42ndturtle@gmail.com>
2018-06-02 15:25:44 -0400

    rfid

--- game.py
+++ game.py
@@ -510,9 +510,9 @@ def rfWrite():
     uid = pn532.read_passive_target()
     while uid is None:
         uid = pn532.read_passive_target()
-    file = os.open("saves/" + str(binascii.hexlify(uid)) + '.json', os.O_RDWR)
-    os.write(file, json.dumps(stuffToSave))
-    os.close(file)
+    with open("saves/" + str(binascii.hexlify(uid)) + '.json', 'w') as file:
+        print(json.dumps(stuffToSave))
+        file.write('test mc man')
 
 
 
