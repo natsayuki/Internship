8b1d4ebdc4646267e66b294f4aafe5f8b212e36c
the42ndturtle <the42ndturtle@gmail.com>
2018-06-02 10:05:42 -0400

    rfid

--- readwrite.py
+++ readwrite.py
@@ -9,15 +9,28 @@ SCLK = 25
 pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
 pn532.SAM_configuration()
 
-print('Waiting for MiFare card...')
+option = input("read or write")
+if option == "read":
 
-while 1:
-    uid = pn532.read_passive_target()
-    if uid is None:
-        continue
-    print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
-    data = pn532.mifare_classic_read_block(4)
-    if data is None:
-        print('Failed to read block 4!')
-        continue
-    print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
+    print('Waiting for MiFare card...')
+
+    while 1:
+        uid = pn532.read_passive_target()
+        if uid is None:
+            continue
+        print('Found card with UID: 0x{0}'.format(binascii.hexlify(uid)))
+        data = pn532.mifare_classic_read_block(4)
+        if data is None:
+            print('Failed to read block 4!')
+            continue
+        print('Read block 4: 0x{0}'.format(binascii.hexlify(data[:4])))
+elif option == "write":
+    if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B, CARD_KEY):
+        print("uh oh no auth")
+    else:
+        data = bytearray(16)
+        data[0:4] = b'test'
+        if not pn532.mifare_classic_write_block(4, data):
+            print("oh no could not write")
+        else:
+            print("write success")
