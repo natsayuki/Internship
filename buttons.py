83e52bc00ede61ad5bf6fc392c9e01ed35a2433a
the42ndturtle <the42ndturtle@gmail.com>
2018-06-04 10:41:08 -0400

    pin swap

--- buttons.py
+++ buttons.py
@@ -13,17 +13,17 @@ GPIO.setup(23, GPIO.IN)
 GPIO.setup(27, GPIO.IN)
 
 while 1:
-    if GPIO.input(17) == 1:
+    if GPIO.input(17) == 0:
         k.press('c')
         k.release('c')
         sleep(.5)
-    elif GPIO.input(22) == 1:
+    elif GPIO.input(22) == 0:
         k.press('m')
         k.release('m')
         sleep(.5)
-    elif GPIO.input(23) == 1:
+    elif GPIO.input(23) == 0:
         k.press('i')
         k.release('i')
         sleep(.5)
-    elif GPIO.input(27) == 1:
+    elif GPIO.input(27) == 0:
         None
