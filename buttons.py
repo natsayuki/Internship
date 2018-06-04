612b216f75c688b1028857354758f19878dba410
the42ndturtle <the42ndturtle@gmail.com>
2018-06-04 11:10:23 -0400

    pin swap

--- buttons.py
+++ buttons.py
@@ -7,23 +7,23 @@ k = Controller()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(22, GPIO.IN)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(27, GPIO.IN)
+GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(27, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 while 1:
-    if GPIO.input(17) == 1:
+    if GPIO.input(17) == 0:
         k.press('c')
         k.release('c')
         sleep(.5)
-    elif GPIO.input(22) == 1:
+    elif GPIO.input(22) == 0:
         k.press('m')
         k.release('m')
         sleep(.5)
-    elif GPIO.input(23) == 1:
+    elif GPIO.input(23) == 0:
         k.press('i')
         k.release('i')
         sleep(.5)
-    elif GPIO.input(27) == 1:
+    elif GPIO.input(27) == 0:
         None
