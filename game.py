00c5f568983751661d6b1b55ad4af489a3b91309
the42ndturtle <the42ndturtle@gmail.com>
2018-06-05 20:34:12 -0400

    class

--- game.py
+++ game.py
@@ -497,6 +497,7 @@ def rfRead():
                 exec(i + " = '" + stuffToLoad[i] + "'")
             else:
                 exec(i + " = " + str(stuffToLoad[i]))
+    print(classPicked + ' class')
     genNewFloor = True
     inGame = True
     inLoad = False
